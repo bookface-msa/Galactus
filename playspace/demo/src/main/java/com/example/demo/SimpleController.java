d3361c03d45a25c9bbed099a67e3419887a2bb55
Omar0Tarek <omar.tarek.elabsawy@gmail.com>
2023-06-02 23:55:41 +0300

    Add logging server

--- playspace/demo/src/main/java/com/example/demo/SimpleController.java
+++ playspace/demo/src/main/java/com/example/demo/SimpleController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(path = "/test")
 public class SimpleController {
+
+	Logger logger = LoggerFactory.getLogger(SpringBootAppApplication.class);
+
     @GetMapping
     public String hello(){
+        logger.info("PLease Work");
         return "hello2";
     }
 }
